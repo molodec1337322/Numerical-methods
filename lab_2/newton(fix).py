759157839e8e2b5b04c14c20fc352d1dd7680a22
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2020-04-25 11:06:05 +0300

    Update newton(fix).py

--- lab_2/newton(fix).py
+++ lab_2/newton(fix).py
@@ -3,11 +3,11 @@ import math
 
 def kramer(x, y):
 
-    main_determinant = math.sin(y - 0.5) * math.cos(x) - 2
+    main_determinant = math.sin(y + 0.5) * math.cos(x) - 2
 
-    first_determinant = -math.sin(y + 0.5) + x + 1 + math.cos(y + 0.5) * math.cos(x - 2) + y * math.cos(y + 0.5)
+    first_determinant = (math.cos(y + 0.5) + x - 0.8) * 2 - ((math.sin(x) - 2 * y - 1.6) * math.sin(y + 0.5))
 
-    second_determinant = math.cos(x - 2) + y - (-math.sin(x - 2) * (-math.sin(y + 0.5) + x + 1))
+    second_determinant = -(math.sin(x) - 2 * y - 1.6) + (math.cos(x) * (math.cos(y + 0.5) + x - 0.8))
 
     delta_x = first_determinant / main_determinant
     delta_y = second_determinant / main_determinant
